96b4f5eac625d0b9adb13c0419df0edd2ee43b05
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 01:13:12 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,24 @@
+package org.cop3330;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is the first number? ");
+        int num1 = in.nextInt();
+        System.out.print("What is the second number? ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+
+        System.out.printf("%d + %d = %d\n", num1, num2, sum);
+        System.out.printf("%d - %d = %d\n", num1, num2, difference);
+        System.out.printf("%d * %d = %d\n", num1, num2, product);
+        System.out.printf("%d / %d = %d\n", num1, num2, quotient);
+    }
+}
